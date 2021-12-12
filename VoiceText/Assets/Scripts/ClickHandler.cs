56339a487d2e7a1ec49a01a8e2c3ac0d34f7bdda
Ghouzali <43846722+GH0GH0@users.noreply.github.com>
2021-12-12 16:52:42 +0100

    1st step SpeechToText

---
+++ VoiceText/Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour
+{
+    public UnityEvent upEvent;
+    public UnityEvent downEvent;
+    public UnityEvent speakEvent;
+
+  /*  public void Start()
+    {
+        speakEvent?.Invoke();
+    }*/
+    public void Update()
+    {
+        speakEvent?.Invoke();
+
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("Down");
+        downEvent?.Invoke();
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Up");
+        upEvent?.Invoke();
+    }
+}
